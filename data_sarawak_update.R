13ae7a07301f271223365aea871445375b289955
wenhao0117 <wenhao0117@hotmail.com>
2022-02-16 11:46:27 +0800

    Update data

--- data_sarawak_update.R
+++ data_sarawak_update.R
@@ -2,34 +2,40 @@ raw <- read.csv('https://raw.githubusercontent.com/MoH-Malaysia/covid19-public/m
 deathRaw <- read.csv('https://raw.githubusercontent.com/MoH-Malaysia/covid19-public/main/epidemic/deaths_state.csv',sep=',',header = TRUE)
 pre <- read.csv('D:/MOZZHUB/COVID-19-Malaysia-Data/srwk_case.csv',sep=",",header = TRUE)
 last <- tail(pre,n=1)
-lastDate <- as.Date(last$Date)
-sar <- raw[raw$state=="Sarawak"&raw$date>lastDate,]
-deathSar <- deathRaw[deathRaw$state=="Sarawak"&deathRaw$date>lastDate,]
-new <- sar$cases_new
-rec <- sar$cases_recovered
-death <- deathSar$deaths_new
-newC <- c()
-recC <- c()
-deaC <- c()
-day <- last$Day+c(1:length(new))
-date <- lastDate+c(1:length(new))
-for(i in 1:length(new)){
-  if(i==1){
-    newC[i] <- last$Positive[1]+new[i]
-    recC[i] <- last$Rec[1]+rec[i]
-    deaC[i] <- last$Death+death[i]
+lastDate <- as.Date(last$Date, format="%m/%d/%Y")
+lastDateRaw <- tail(raw[raw$state=="Sarawak",],n=1)$date
+if(lastDate==lastDateRaw){
+  print("All Updated")
+} else{
+  sar <- raw[raw$state=="Sarawak"&raw$date>lastDate,]
+  deathSar <- deathRaw[deathRaw$state=="Sarawak"&deathRaw$date>lastDate,]
+  new <- sar$cases_new
+  rec <- sar$cases_recovered
+  death <- deathSar$deaths_new
+  newC <- c()
+  recC <- c()
+  deaC <- c()
+  day <- last$Day+c(1:length(new))
+  date <- lastDate+c(1:length(new))
+  for(i in 1:length(new)){
+    if(i==1){
+      newC[i] <- last$Positive[1]+new[i]
+      recC[i] <- last$Rec[1]+rec[i]
+      deaC[i] <- last$Death+death[i]
+    }
+    else{
+      newC[i] <- newC[i-1]+new[i]
+      recC[i] <- recC[i-1]+rec[i]
+      deaC[i] <- deaC[i-1]+death[i]  
+    }
   }
-  else{
-    newC[i] <- newC[i-1]+new[i]
-    recC[i] <- recC[i-1]+rec[i]
-    deaC[i] <- deaC[i-1]+death[i]  
-  }
+  case <- newC-recC-deaC
+  
+  newRow <- data.frame(day,date,newC,recC,deaC,case)
+  
+  write.table(newRow,file = 'D:/MOZZHUB/COVID-19-Malaysia-Data/srwk_case.csv',sep = ',', 
+              append = T, 
+              row.names=F, 
+              col.names=F)
+  
 }
-case <- newC-recC-deaC
-
-newRow <- data.frame(day,date,newC,recC,deaC,case)
-
-write.table(newRow,file = 'D:/MOZZHUB/COVID-19-Malaysia-Data/srwk_case.csv',sep = ',', 
-            append = T, 
-            row.names=F, 
-            col.names=F)
